6c84a8296d6c5662099aa729fe1a54bf9a5beb24
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-05-25 14:15:19 +0530

    [Debashis] Checking Null and Empty input

---
+++ MoodAnalyzerUsingMSTest/MoodAnalysisExceptionTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer.Tests
+{
+    [TestClass]
+    public class MoodAnalysisExceptionTest
+    {
+        [TestMethod]
+        public void MoodAnalysisExceptionMethodTest1()
+        {
+            try
+            {
+                string message = "";
+                AnalyzemoodException moodAnalysisException = new AnalyzemoodException();
+                string actual = moodAnalysisException.AnalyzeMoodExceptionMethod (message);
+            }
+            catch (MoodCustomException ex)
+            {
+
+                Assert.AreEqual("Mood should not be empty", ex.Message);
+            }
+        }
+
+
+
+        [TestMethod]
+        public void MoodAnalysisExceptionMethodTest2()
+        {
+            try
+            {
+                string message = null;
+                AnalyzemoodException moodAnalysisException = new AnalyzemoodException();
+                string actual = moodAnalysisException.AnalyzeMoodExceptionMethod(message);
+            }
+            catch (MoodCustomException ex)
+            {
+                Assert.AreEqual("Mood should not be null", ex.Message);
+            }
+        }
+    }
+
+}
