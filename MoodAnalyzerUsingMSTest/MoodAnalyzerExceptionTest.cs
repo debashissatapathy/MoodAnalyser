b12ef97d90e7d1c53cba405e94cb0d71fc941ecd
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-05-25 14:28:04 +0530

    [Debashis] User custom exception Testcase

---
+++ MoodAnalyzerUsingMSTest/MoodAnalyzerExceptionTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer.Test
+{
+    [TestClass]
+    public class MoodAnalyzerExceptionTest
+    {
+        [TestMethod]
+        public void MoodAnalyzerExceptionMethodTest()
+        {
+            MoodAnalysisException moodAnalysis = new MoodAnalysisException();
+            string mood = moodAnalysis.MoodAnalysisExceptionMethod("");
+            Assert.AreEqual("SAD", mood);
+        }
+    }
+}
