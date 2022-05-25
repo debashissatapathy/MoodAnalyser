f3e1567b1e78fbe062333136d293c35a197cf0d6
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-05-25 14:01:24 +0530

    [Debashis] Using default and paramiterize constructor testcase

---
+++ MoodAnalyzerUsingMSTest/AnalyseMoodUsingConstuctorTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer.Test
+{
+    [TestClass]
+    public class AnalyseMoodUsingConstuctorTest
+    {
+        
+        [TestMethod]
+        public void AnalyseMoodUsingConstuctorTest1()
+        {
+            AnalyzeMoodUsingConstructor A1 = new AnalyzeMoodUsingConstructor();
+            string mood = A1.AnalyseMoodMethod();
+            Assert.AreEqual("SAD", mood);
+        }
+        [TestMethod]
+        public void AnalyseMoodUsingConstuctorTest2()
+        {
+            AnalyzeMoodUsingConstructor A2 = new AnalyzeMoodUsingConstructor("I am in any mood");
+            string mood = A2.AnalyseMoodMethod();
+            Assert.AreEqual("HAPPY", mood);
+        }
+    }
+}
